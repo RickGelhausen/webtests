9e71574203995541b415cd0d090af601bc312963
RickGelhausen <rick.gelhausen@gmail.com>
2023-07-05 22:55:58 +0200

    updated builddocs.sh

--- scripts/builddocs.sh
+++ scripts/builddocs.sh
@@ -11,9 +11,8 @@ export SOURCE_DATE_EPOCH=$(git log -1 --pretty=%ct)
 # Install Jekyll #
 ##################
 
-echo 'export GEM_HOME="$HOME/gems"' >> ~/.bashrc
-echo 'export PATH="$HOME/gems/bin:$PATH"' >> ~/.bashrc
-source ~/.bashrc
+export GEM_HOME="$HOME/gems"
+export PATH="$HOME/gems/bin:$PATH"
 
 gem install jekyll bundler
 
